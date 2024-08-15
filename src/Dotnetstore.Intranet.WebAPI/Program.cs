b4a1a7b0e33943070340e96fae48f257d5813723
Hans Sjödin <hasse29@hotmail.com>
2024-08-15 07:30:58 +0200

    Added global exception handler.

--- src/Dotnetstore.Intranet.WebAPI/Program.cs
+++ src/Dotnetstore.Intranet.WebAPI/Program.cs
@@ -1,29 +1,17 @@
-using Dotnetstore.Intranet.Organization;
-using Dotnetstore.Intranet.Utility.Extensions;
+using Dotnetstore.Intranet.WebAPI;
 using FastEndpoints;
 using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddFastEndpoints()
-    .SwaggerDocument(o =>
-    {
-        o.DocumentSettings = s =>
-        {
-            s.Title = "Dotnetstore Intranet API";
-            s.Description = "API for Dotnetstore Intranet";
-            s.Version = "1.0";
-        };
-    })
-    .AddUtility()
-    .AddOrganization(builder.Configuration);
+builder.Services.AddWebApi(builder.Configuration);
     
 var app = builder.Build();
 
 app
     .UseFastEndpoints()
-    .UseSwaggerGen();
+    .UseSwaggerGen()
+    .UseExceptionHandler();
     
 await app.RunAsync();
 
---
+++ src/Dotnetstore.Intranet.WebAPI/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Dotnetstore.Intranet.Organization;
+using Dotnetstore.Intranet.Utility.Extensions;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+namespace Dotnetstore.Intranet.WebAPI;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddWebApi(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddFastEndpoints()
+            .SwaggerDocument(o =>
+            {
+                o.DocumentSettings = s =>
+                {
+                    s.Title = "Dotnetstore Intranet API";
+                    s.Description = "API for Dotnetstore Intranet";
+                    s.Version = "1.0";
+                };
+            })
+            .AddUtility()
+            .AddOrganization(configuration)
+            .AddExceptionHandler<GlobalExceptionHandler>()
+            .AddProblemDetails();
+
+        return services;
+    }
+}
